572b7a5443e34619a0cefb4515d0e78442db0264
Yevhen Tiurin <yevhentiurin@gmail.com>
2016-08-06 21:11:19 +0300

    Update Bengali patterns

--- patterns/bn.js
+++ patterns/bn.js
@@ -19,8 +19,12 @@
 
   // Hyphenation patterns
   var patterns=`
+% GENERAL RULE
+% Do not break either side of ZERO-WIDTH JOINER  (U+200D)
 2‍2
+% Break on both sides of ZERO-WIDTH NON JOINER  (U+200C)
 1‌1
+% Break before or after any independent vowel.
 অ1
 আ1
 ই1
@@ -35,6 +39,7 @@
 ঐ1
 ও1
 ঔ1
+% Break after any dependent vowel, but not before.
 া1
 ি1
 ী1
@@ -50,6 +55,7 @@
 ৌ1
 2়2
 ৗ1
+% Break before or after any consonant.
 1ক
 1খ
 1গ
@@ -85,11 +91,15 @@
 1ষ
 1স
 1হ
+% Do not break after khanda ta.
 ৎ1
+% Do not break before chandrabindu, anusvara, visarga, avagraha,
+% nukta and au length mark.
 2ঃ1
 2ং1
 2ঁ1
 2ঽ1
+% Do not break either side of virama (may be within conjunct).
 2্2
 `;
 
