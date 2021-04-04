9c99f8f97502aaae9ef98a6c010b8325b08f352a
Yevhen Tiurin <yevhentiurin@gmail.com>
2021-04-04 13:38:02 +0100

    Add test case for minWordLength option

--- tests/min-word-length.test.js
+++ tests/min-word-length.test.js
@@ -8,15 +8,19 @@ beforeAll(() => {
 });
 
 describe("Minimum word length hyphenation", () => {
-  test("Should hyphenate long word", () => {
+  test("Should hyphenate word of length >= 5 by default", () => {
     expect(hyphenate("beautiful")).toBe("beau-ti-ful");
   });
 
-  test("Should NOT hyphenate long word when option minWordLength = 9", () => {
+  test("Should NOT hyphenate word of length 9, when option minWordLength = 10", () => {
     expect(hyphenate("beautiful", { minWordLength: 10 })).toBe("beautiful");
   });
 
-  test("Should NOT hyphenate short word of 4 chars", () => {
+  test("Should NOT hyphenate word of length < 5 by default", () => {
     expect(hyphenate("lady")).toBe("lady");
+  });
+
+  test("Should hyphenate word of length 4, when option minWordLength = 4", () => {
+    expect(hyphenate("lady", { minWordLength: 4 })).toBe("la-dy");
   });
 });
