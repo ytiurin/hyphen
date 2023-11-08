8f731b286a81769c0d5d42c7404d8a6789d9c26a
Eugene Tiurin <yevhentiurin@gmail.com>
2023-11-08 21:47:32 +0200

    Fixed broken patterns exceptions

--- dev/hyphen.js
+++ dev/hyphen.js
@@ -3,7 +3,7 @@ import { createRequire } from "module";
 
 let text =
   "The Tortoise never stopped for a moment, walking slowly but steadily, right to the end of the course. The Hare ran fast and stopped to lie down for a rest. But he fell fast asleep. Eventually, he woke up and ran as fast as he could. But when he reached the end, he saw the Tortoise there already, sleeping comfortably after her effort.";
-// text = "hyphenation hyphenation";
+// text = "hyphenation hyphenation table project";
 
 const require = createRequire(import.meta.url);
 const enUS = require("../patterns/en-us.cjs");
