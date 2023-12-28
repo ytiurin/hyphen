15a348fd17cbed0d2fe307eceddb219653b0b201
Eugene Tiurin <yevhentiurin@gmail.com>
2023-12-28 21:42:38 +0200

    BREAKING CHANGE: Option `html` default value changed to `true`

--- tests/html.test.js
+++ tests/html.test.js
@@ -1,25 +1,20 @@
 const createHyphenator = require("../hyphen.js");
 const patterns = require("../patterns/en-us.js");
 
-let hyphenate, hyphenateHTML;
+let hyphenate;
 
 beforeAll(() => {
   hyphenate = createHyphenator(patterns, { hyphenChar: "-" });
-  hyphenateHTML = createHyphenator(patterns, { html: true, hyphenChar: "-" });
 });
 
 describe("HTML syntax hyphenation", () => {
-  test("Should hyphenate HTML syntax by default", () => {
-    expect(hyphenate("<beautiful>")).toBe("<beau-ti-ful>");
-  });
-
   test("Should NOT hyphenate HTML syntax, if `html` option set to true", () => {
-    expect(hyphenateHTML('<beautiful class="beautiful">')).toBe(
+    expect(hyphenate('<beautiful class="beautiful">')).toBe(
       '<beautiful class="beautiful">'
     );
   });
 
   test("Should NOT recognize text as HTML, if `<` char is followed by space", () => {
-    expect(hyphenateHTML("< beautiful >")).toBe("< beau-ti-ful >");
+    expect(hyphenate("< beautiful >")).toBe("< beau-ti-ful >");
   });
 });
