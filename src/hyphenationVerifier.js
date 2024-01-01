2275ef81d46656d1d672560b2f948693a976cf83
Eugene Tiurin <yevhentiurin@gmail.com>
2024-01-01 23:23:33 +0200

    Destructured text reader into composable verifiers

--- src/hyphenationVerifier.js
+++ src/hyphenationVerifier.js
@@ -2,18 +2,57 @@ var isNotLetter = RegExp.prototype.test.bind(
   /\s|(?![\'])[\!-\@\[-\`\{-\~\u2013-\u203C]/
 );
 
-export function createHyphenationVerifier(hyphenChar, skipHTML, minWordLength) {
+export function createHTMLVerifier(skipHTML) {
+  var skip = false;
+
+  return function (accumulate, chars) {
+    if (skip) {
+      if (chars[0] === ">") {
+        accumulate();
+        skip = false;
+      }
+    } else if (
+      chars[0] === "<" &&
+      (!isNotLetter(chars[1]) || chars[1] === "/") &&
+      skipHTML
+    ) {
+      skip = true;
+    }
+
+    return skip;
+  };
+}
+
+export function createHyphenCharVerifier(hyphenChar) {
+  var skip = false;
+
+  return function (accumulate, chars) {
+    if (skip) {
+      if (!isNotLetter(chars[0]) && isNotLetter(chars[1])) {
+        accumulate();
+        skip = false;
+      }
+    } else if (!isNotLetter(chars[0]) && chars[1] === hyphenChar) {
+      skip = true;
+    }
+
+    return skip;
+  };
+}
+
+export function createHyphenationVerifier(verifiers, minWordLength) {
   return function () {
     var accum0 = "";
     var accum = "";
-    var isHTMLTag = false;
-    var skipCurrent = false;
+
+    function accumulate() {
+      accum0 += accum;
+      accum = "";
+    }
 
     function resolveWith(value) {
       accum0 = "";
       accum = "";
-      isHTMLTag = false;
-      skipCurrent = false;
 
       return value;
     }
@@ -21,44 +60,27 @@ export function createHyphenationVerifier(hyphenChar, skipHTML, minWordLength) {
     return function (char1, char2) {
       accum += char1;
 
-      if (isHTMLTag) {
-        if (char1 === ">") {
-          accum0 += accum;
-          accum = "";
-          isHTMLTag = false;
-        }
-      } else {
-        if (char1 === hyphenChar) {
-          skipCurrent = true;
-        }
+      var skip = verifiers.reduce(function (skip, verify) {
+        return skip || verify(accumulate, [char1, char2]);
+      }, false);
 
-        if (
-          char1 === "<" &&
-          (!isNotLetter(char2) || char2 === "/") &&
-          skipHTML
-        ) {
-          isHTMLTag = true;
-        }
-
+      if (!skip) {
         if (isNotLetter(char1) && !isNotLetter(char2)) {
-          accum0 += accum;
-          accum = "";
+          accumulate();
         }
 
         if (!isNotLetter(char1) && isNotLetter(char2)) {
-          if (accum.length >= minWordLength && !skipCurrent) {
+          if (accum.length >= minWordLength) {
             return resolveWith([accum0, accum]);
           } else {
-            accum0 += accum;
-            accum = "";
+            accumulate();
           }
         }
       }
 
       if (char2 === "") {
-        if (accum.length < minWordLength || skipCurrent) {
-          accum0 += accum;
-          accum = "";
+        if (accum.length < minWordLength || skip) {
+          accumulate();
         }
 
         return resolveWith([accum0, accum]);
