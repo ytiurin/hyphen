ae3ff9d07f60ac7a89c7344b4fdfc43f54e4cfac
Yevhen Tiurin <yevhentiurin@gmail.com>
2024-09-12 18:14:47 +0100

    Fixed the issue with bad hyphenation, that started in version 1.7.0

--- tests/basic-text.test.js
+++ tests/basic-text.test.js
@@ -12,7 +12,7 @@ describe("Basic text", () => {
     const basicText =
       "The Tortoise never stopped for a moment, walking slowly but steadily, right to the end of the course. The Hare ran fast and stopped to lie down for a rest. But he fell fast asleep. Eventually, he woke up and ran as fast as he could. But when he reached the end, he saw the Tortoise there already, sleeping comfortably after her effort.";
     const predictable =
-      "The Tor-toise never stopped for a mo-ment, walk-ing slowly but steadily, right to the end of the course. The Hare ran fast and stopped to lie down for a rest. But he fell fast asleep. Even-tu-al-ly, he woke up and ran as fast as he could. But when he reached the end, he saw the Tor-toise there al-ready, sleep-ing com-fort-ably af-ter her ef-fort.";
+      "The Tor-toise nev-er stopped for a mo-ment, walk-ing slow-ly but steadi-ly, right to the end of the course. The Hare ran fast and stopped to lie down for a rest. But he fell fast asleep. Even-tu-al-ly, he woke up and ran as fast as he could. But when he reached the end, he saw the Tor-toise there al-ready, sleep-ing com-fort-ably af-ter her ef-fort.";
 
     expect(hyphenate(basicText)).toBe(predictable);
   });
